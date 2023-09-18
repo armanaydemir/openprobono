96e20d3a3e58d9d0e8bf47ebc7299843b2e6249f
Arman Aydemir <arman.aydemir@live.com>
2023-09-18 15:47:08 -0700

    Update app_start script bc of name change

--- app_start.sh
+++ app_start.sh
@@ -2,4 +2,4 @@
 
 conda activate h2ogpt
 
-OPENAI_API_KEY=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 nohup python client.py &
+OPENAI_API_KEY=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 nohup python app.py &

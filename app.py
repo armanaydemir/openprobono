746cee503d827ea681e97d75fe0203deaeeeceb9
Arman Aydemir <arman.aydemir@live.com>
2023-09-27 15:55:13 -0700

    making gpt3.5 solely for public facing site

--- app.py
+++ app.py
@@ -161,43 +161,43 @@ class SagemakerAsyncEndpoint(SagemakerEndpoint):
 system_prompt = """You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
 # too much safety, hurts accuracy
 
-class AsyncContentHandler(LLMContentHandler):
-    content_type:str = "application/json"
-    accepts:str = "application/json"
-    len_prompt:int = 0
+# class AsyncContentHandler(LLMContentHandler):
+#     content_type:str = "application/json"
+#     accepts:str = "application/json"
+#     len_prompt:int = 0
 
-    def transform_input(self, prompt: str, model_kwargs: Dict) -> bytes:
-        self.len_prompt = len(prompt)
-        input_str = json.dumps({"inputs": prompt, "parameters": {"max_new_tokens": 1000, "top_p": 0.6, "temperature": 0.1},})
-        return input_str.encode('utf-8')
+#     def transform_input(self, prompt: str, model_kwargs: Dict) -> bytes:
+#         self.len_prompt = len(prompt)
+#         input_str = json.dumps({"inputs": prompt, "parameters": {"max_new_tokens": 1000, "top_p": 0.6, "temperature": 0.1},})
+#         return input_str.encode('utf-8')
 
-    def transform_output(self, output: bytes) -> str:
-        response_json = output.read()
-        res = json.loads(response_json)
-        ans = res[0]['generated_text']
-        return ans
+#     def transform_output(self, output: bytes) -> str:
+#         response_json = output.read()
+#         res = json.loads(response_json)
+#         ans = res[0]['generated_text']
+#         return ans
 
-class ContentHandler(LLMContentHandler):
-    content_type = "application/json"
-    accepts = "application/json"
+# class ContentHandler(LLMContentHandler):
+#     content_type = "application/json"
+#     accepts = "application/json"
 
-    def transform_input(self, prompt: str, model_kwargs: Dict) -> bytes:
-        payload = {
-            "inputs": [
-                [
-                 {"role": "user", "content": prompt}],
-                ],
-                "parameters": {"max_new_tokens": 1000, "top_p": 0.6, "temperature": 0.1},
-        }
+#     def transform_input(self, prompt: str, model_kwargs: Dict) -> bytes:
+#         payload = {
+#             "inputs": [
+#                 [
+#                  {"role": "user", "content": prompt}],
+#                 ],
+#                 "parameters": {"max_new_tokens": 1000, "top_p": 0.6, "temperature": 0.1},
+#         }
 
-        input_str = json.dumps(
-            payload,
-        )
-        return input_str.encode("utf-8")
+#         input_str = json.dumps(
+#             payload,
+#         )
+#         return input_str.encode("utf-8")
 
-    def transform_output(self, output: bytes) -> str:
-        response_json = json.loads(output.read().decode("utf-8"))
-        return response_json[0]["generation"]["content"]
+#     def transform_output(self, output: bytes) -> str:
+#         response_json = json.loads(output.read().decode("utf-8"))
+#         return response_json[0]["generation"]["content"]
 
 
 
@@ -215,14 +215,14 @@ with gr.Blocks(title="OpenProBono",
     #         content_handler=content_handler,
     #     )
 
-    async_content_handler = AsyncContentHandler()
+    # async_content_handler = AsyncContentHandler()
 
-    async_endpoint = SagemakerAsyncEndpoint(
-        endpoint_name="huggingface-pytorch-tgi-inference-2023-09-25-05-08-31-975",
-        region_name=sagemaker.Session().boto_region_name,
-        model_kwargs={"temperature": 1e-10},
-        content_handler=async_content_handler,
-    )
+    # async_endpoint = SagemakerAsyncEndpoint(
+    #     endpoint_name="huggingface-pytorch-tgi-inference-2023-09-25-05-08-31-975",
+    #     region_name=sagemaker.Session().boto_region_name,
+    #     model_kwargs={"temperature": 1e-10},
+    #     content_handler=async_content_handler,
+    # )
 
     gpt3_llm = ChatOpenAI(temperature=1.0, model='gpt-3.5-turbo-0613')
     
@@ -238,22 +238,22 @@ with gr.Blocks(title="OpenProBono",
         history = history + [((file.name,), None)]
         return history
 
-    def async_bot(history, context):
-        PROMPT = ""
-        if context != "":
-            PROMPT += "Pay attention and remember information below, which will help to answer the question or imperative after the context ends.\n"
-            PROMPT += context
-            PROMPT += "\nReference the information in the document sources provided within the context above.\n"
-        PROMPT += "The following is a conversation between a human and an AI. The AI is a helpful assistant. If the AI does not know the answer to a question, it truthfully says it does not know.\n\n"
-        PROMPT += "Current conversation:\n"
-        for i in range(0, len(history)-1):
-            (human, ai) = history[i]
-            PROMPT += "[INST] " + human + " [/INST]\n"
-            PROMPT += ai
-        PROMPT += "[INST] " + history[-1][0] + " [/INST]\n"
-        history[-1][1] = async_endpoint(PROMPT).split("\n")[-1]
+    # def async_bot(history, context):
+    #     PROMPT = ""
+    #     if context != "":
+    #         PROMPT += "Pay attention and remember information below, which will help to answer the question or imperative after the context ends.\n"
+    #         PROMPT += context
+    #         PROMPT += "\nReference the information in the document sources provided within the context above.\n"
+    #     PROMPT += "The following is a conversation between a human and an AI. The AI is a helpful assistant. If the AI does not know the answer to a question, it truthfully says it does not know.\n\n"
+    #     PROMPT += "Current conversation:\n"
+    #     for i in range(0, len(history)-1):
+    #         (human, ai) = history[i]
+    #         PROMPT += "[INST] " + human + " [/INST]\n"
+    #         PROMPT += ai
+    #     PROMPT += "[INST] " + history[-1][0] + " [/INST]\n"
+    #     history[-1][1] = async_endpoint(PROMPT).split("\n")[-1]
 
-        yield history
+    #     yield history
 
     #actually generates the text and uses langchain (this is where handoff between frontend and backend is)
     # def bot(history, context):
@@ -313,13 +313,13 @@ with gr.Blocks(title="OpenProBono",
 
     gr.Markdown("OpenProBono")
     with gr.Row():
-        async_chat = gr.Chatbot(
-            [],
-            elem_id="llama2-7b-chat",
-            label="llama2-7b-chat",
-            #bubble_full_width=True,
-            #avatar_images=(None, (os.path.join(os.path.dirname(__file__), "avatar.png"))),
-        )
+        # async_chat = gr.Chatbot(
+        #     [],
+        #     elem_id="llama2-7b-chat",
+        #     label="llama2-7b-chat",
+        #     #bubble_full_width=True,
+        #     #avatar_images=(None, (os.path.join(os.path.dirname(__file__), "avatar.png"))),
+        # )
         # sage_chat = gr.Chatbot(
         #     [],
         #     elem_id="sagemaker-llama",
@@ -330,7 +330,7 @@ with gr.Blocks(title="OpenProBono",
         openai_chat = gr.Chatbot(
             [],
             elem_id="gpt3.5-turbo",
-            label="gpt3.5",
+            label="OpenProBono - gpt3.5",
             #bubble_full_width=True,
             #avatar_images=(None, (os.path.join(os.path.dirname(__file__), "avatar.png"))),
         )
@@ -354,7 +354,7 @@ with gr.Blocks(title="OpenProBono",
         #btn = gr.UploadButton("📁", file_types=["text"])
 
     with gr.Row():
-        clearasync = gr.ClearButton([txt, async_chat])
+        # clearasync = gr.ClearButton([txt, async_chat])
         # clearsage = gr.ClearButton([txt, sage_chat])
         clearopenai = gr.ClearButton([txt, openai_chat])
 
@@ -362,9 +362,9 @@ with gr.Blocks(title="OpenProBono",
     # txt_msg = txt.submit(add_text, [sage_chat, txt], [sage_chat, txt], queue=False).then(
     #     bot, [sage_chat, contxt], sage_chat
     # )
-    txt_msg = txt.submit(add_text, [async_chat, txt], [async_chat, txt], queue=False).then(
-        async_bot, [async_chat, contxt], async_chat
-    )
+    # txt_msg = txt.submit(add_text, [async_chat, txt], [async_chat, txt], queue=False).then(
+    #     async_bot, [async_chat, contxt], async_chat
+    # )
     txt_msg = txt.submit(add_text, [openai_chat, txt], [openai_chat, txt], queue=False).then(
         openai_bot, [openai_chat, contxt], openai_chat
     )
@@ -374,9 +374,9 @@ with gr.Blocks(title="OpenProBono",
     # sub_msg = subbtn.click(add_text, [sage_chat, txt], [sage_chat, txt], queue=False).then(
     #     bot, [sage_chat, contxt], sage_chat
     # )
-    sub_msg = subbtn.click(add_text, [async_chat, txt], [async_chat, txt], queue=False).then(
-        async_bot, [async_chat, contxt], async_chat
-    )
+    # sub_msg = subbtn.click(add_text, [async_chat, txt], [async_chat, txt], queue=False).then(
+    #     async_bot, [async_chat, contxt], async_chat
+    # )
     sub_msg = subbtn.click(add_text, [openai_chat, txt], [openai_chat, txt], queue=False).then(
         openai_bot, [openai_chat, contxt], openai_chat
     )

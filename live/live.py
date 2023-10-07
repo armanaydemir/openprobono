2f9b181a51434e5c5062d978c919733d8d4582d8
Arman Aydemir <arman.aydemir@live.com>
2023-10-07 01:41:02 -0700

    agent debug

--- live/live.py
+++ live/live.py
@@ -143,6 +143,7 @@ with gr.Blocks(title="Workspace",
             agent_kwargs=agent_kwargs,
             memory=memory,
         )
+        print(agent)
         bot_message = agent.run(history[-1][0])
         history[-1][1] = bot_message
         yield history

c405ff44c2cc3cfdbaf1484b47870466fb9717a9
Arman Aydemir <arman.aydemir@live.com>
2023-10-17 14:56:13 -0700

    trying planning/observation

--- staging/staging.py
+++ staging/staging.py
@@ -71,6 +71,106 @@ def general_search(q):
         'num': 5
         }).get_dict())
 
+#defining the tools available to the agent
+tools = [
+    Tool(
+        name="search",
+        func=general_search,
+        description="useful for when you need to answer questions about current events. You should ask targeted questions. Always cite your sources.",
+    ),
+    Tool(
+        name="government-search",
+        func=gov_search,
+        description="useful for when you need to answer questions or find resources about government and laws. Always cite your sources.",
+    )
+]
+
+tool_names = [tool.name for tool in tools]
+
+# Set up the base template
+template = """Complete the user's request as best you can. You have access to the following tools:
+
+{tools}
+
+The following is the chat history so far:
+{memory}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+These were previous tasks you completed:
+
+
+
+Begin!
+
+{input}
+{agent_scratchpad}"""
+
+# Set up a prompt template
+class CustomPromptTemplate(BaseChatPromptTemplate):
+    # The template to use
+    template: str
+    # The list of tools available
+    tools: List[Tool]
+
+    def format_messages(self, **kwargs) -> str:
+        # Get the intermediate steps (AgentAction, Observation tuples)
+        # Format them in a particular way
+        intermediate_steps = kwargs.pop("intermediate_steps")
+        thoughts = ""
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f"\nObservation: {observation}\nThought: "
+        # Set the agent_scratchpad variable to that value
+        kwargs["agent_scratchpad"] = thoughts
+        # Create a tools variable from the list of tools provided
+        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+        # Create a list of tool names for the tools provided
+        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        formatted = self.template.format(**kwargs)
+        return [HumanMessage(content=formatted)]
+    
+class CustomOutputParser(AgentOutputParser):
+
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        # Return the action and action input
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+    
+
+
+prompt = CustomPromptTemplate(
+    template=template,
+    tools=tools,
+    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+    # This includes the `intermediate_steps` variable because that is needed
+    input_variables=["input", "intermediate_steps"]
+)
+
+output_parser = CustomOutputParser()
 
 # system_prompt = """You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
 # too much safety, hurts accuracy
@@ -107,20 +207,6 @@ with gr.Blocks(title="OpenProBono",
 
     #this is the meat, where the chat history is passed and the bot responds
     def openai_bot(history):
-        #defining the tools available to the agent
-        tools = [
-            Tool(
-                name="search",
-                func=general_search,
-                description="useful for when you need to answer questions about current events. You should ask targeted questions. Always cite your sources.",
-            ),
-            Tool(
-                name="government-search",
-                func=gov_search,
-                description="useful for when you need to answer questions or find resources about government and laws. Always cite your sources.",
-            )
-        ]
-        
         #defining the memory of the agent
         history_langchain_format = ChatMessageHistory()
         for i in range(0, len(history)-1):
@@ -138,9 +224,10 @@ with gr.Blocks(title="OpenProBono",
         agent_kwargs = {
             "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
         }
+        llm_chain = LLMChain(llm=gpt3_llm, prompt=prompt)
         agent = initialize_agent(
             tools=tools,
-            llm=gpt3_llm,
+            llm=llm_chain,
             agent=AgentType.OPENAI_FUNCTIONS,
             verbose=True,
             agent_kwargs=agent_kwargs,

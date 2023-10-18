00753ad3eb518342e1f680a7707cae0818353d1b
Arman Aydemir <arman.aydemir@live.com>
2023-10-17 22:23:12 -0700

    changing serpapi key

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ langchain.debug = True
 
 from serpapi import GoogleSearch
 
-GoogleSearch.SERP_API_KEY = "5567e356a3e19133465bc68755a124268543a7dd0b2809d75b038797b43626ab"
+GoogleSearch.SERP_API_KEY = "e6e9a37144cdd3e3e40634f60ef69c1ea6e330dfa0d0cde58991aa2552fff980"
 
 def filtered_search(results):
     new_dict = {}

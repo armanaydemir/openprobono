727aecee4cc5c019508b7fe84593e4810fd30638
Arman Aydemir <arman.aydemir@live.com>
2023-10-18 19:51:24 -0700

    finishing organizing app.py, still need to potentially debug

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import gradio as gr
-import os
-
-
 import langchain
 from langchain import PromptTemplate
 from langchain.agents import AgentExecutor, AgentType, initialize_agent, Tool, ZeroShotAgent
@@ -11,7 +8,7 @@ from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory, ChatMessageHistory
 from langchain.prompts import MessagesPlaceholder
 from langchain.schema import AIMessage, HumanMessage
-
+import os
 from serpapi import GoogleSearch
 
 
@@ -43,7 +40,7 @@ def gov_search(q):
         'num': 5
         }).get_dict())
 
-#Filter Search results retured by serpapi to only include relavant results
+#Filter search results retured by serpapi to only include relavant results
 def filtered_search(results):
     new_dict = {}
     if('sports_results' in results):
@@ -66,66 +63,55 @@ tools = [
     )
 ]
 
+#definition of llm used for bot
+bot_llm = ChatOpenAI(temperature=0.0, model='gpt-3.5-turbo-0613')
+
+def print_email(email):
+        print(email)
+        print("^^ this is the email ^^")
+        return email
+
+def openai_bot(history):
+    history_langchain_format = ChatMessageHistory()
+    for i in range(0, len(history)-1):
+        (human, ai) = history[i]
+        history_langchain_format.add_user_message(human)
+        history_langchain_format.add_ai_message(ai)
+    memory = ConversationBufferMemory(return_messages=True, chat_memory=history_langchain_format, memory_key="memory")
+
+    system_message = 'You are a helpful AI assistant. '
+    #system_message += user_prompt
+    system_message += '. ALWAYS return a "SOURCES" part in your answer.'
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+    }
+    agent = initialize_agent(
+        tools=tools,
+        llm=bot_llm,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        verbose=True,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+    )
+    agent.agent.prompt.messages[0].content = system_message
+    bot_message = agent.run(history[-1][0])
+    history[-1][1] = bot_message
+    yield history
+
 with gr.Blocks(
     title = "OpenProBono",
     theme = gr.themes.Default(
         primary_hue = gr.themes.colors.indigo, 
         secondary_hue = gr.themes.colors.blue,
         font = gr.themes.GoogleFont("Open Sans"),
-        radius_size =g r.themes.sizes.radius_lg,
+        radius_size = gr.themes.sizes.radius_lg,
     ),
     css = "footer {visibility: hidden}"
     ) as app:
-
-    #definition of llm used for bot
-    bot_llm = ChatOpenAI(temperature=0.0, model='gpt-3.5-turbo-0613')
-
-    def print_email(email):
-        print(email)
-        print("^^ this is the email ^^")
-        return email
     
     def add_text(history, text):
         history = history + [(text, None)]
         return history, gr.update(value="", interactive=False)
-
-
-    def add_file(history, file):
-        loader = TextLoader(file.name)
-        file_text = loader.load()
-
-        history = history + [((file.name,), None)]
-        return history
-
-
-    def openai_bot(history):
-        history_langchain_format = ChatMessageHistory()
-        for i in range(0, len(history)-1):
-            (human, ai) = history[i]
-            history_langchain_format.add_user_message(human)
-            history_langchain_format.add_ai_message(ai)
-        memory = ConversationBufferMemory(return_messages=True, chat_memory=history_langchain_format, memory_key="memory")
-
-        system_message = 'You are a helpful AI assistant. '
-        #system_message += user_prompt
-        system_message += '. ALWAYS return a "SOURCES" part in your answer.'
-        agent_kwargs = {
-            "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
-        }
-        agent = initialize_agent(
-            tools=tools,
-            llm=bot_llm,
-            agent=AgentType.OPENAI_FUNCTIONS,
-            verbose=True,
-            agent_kwargs=agent_kwargs,
-            memory=memory,
-        )
-        agent.agent.prompt.messages[0].content = system_message
-        bot_message = agent.run(history[-1][0])
-        history[-1][1] = bot_message
-        yield history
-    
-
 
     gr.Markdown("OpenProBono")
     with gr.Row():
@@ -134,8 +120,6 @@ with gr.Blocks(
             elem_id="chat",
             label="OpenProBono",
             show_label=True,
-            #bubble_full_width=True,
-            #avatar_images=(None, (os.path.join(os.path.dirname(__file__), "avatar.png"))),
         )
 
     with gr.Row():
@@ -143,39 +127,39 @@ with gr.Blocks(
             scale=4,
             label="input",
             show_label=False,
-            placeholder="Enter query", #, or upload an image",
+            placeholder="Enter query",
             container=False,
         )
         subbtn = gr.Button("Submit", variant="primary")
         clearopenai = gr.ClearButton([txt, openai_chat])
-        #btn = gr.UploadButton("📁", file_types=["text"])
-
-
-    # file_msg = btn.upload(add_file, [openai, btn], [openai], queue=False).then(
-    #     bot, openai, openai
-    # )
+       
     with gr.Accordion("Details"):
         with gr.Row():
             emailtxt = gr.Textbox(
                 scale=4,
                 label="input",
                 show_label=False,
-                placeholder="Enter your email to sign up for updates", #, or upload an image",
+                placeholder="Enter your email to sign up for updates",
                 container=False,
             )
             emailbtn = gr.Button("Submit")
         gr.Markdown("This demo is a beta meant for informational purposes, demonstrating the abilities of our current technology and to compare different variations of models, prompting methods, document upload, and other features as we continually improve. The data sent in the demo is not guaranteed to be kept private. We will keep iterating on this demo, so keep an eye out for frequent updates. This is not legal advice. Learn more at www.openprobono.com.")
 
+    #connecting frontend interactions to backend
+
+    #corresponds to enter in the text box
     txt_msg = txt.submit(add_text, [openai_chat, txt], [openai_chat, txt], queue=False).then(
         openai_bot, [openai_chat], openai_chat
     )
     txt_msg.then(lambda: gr.update(interactive=True), None, [txt], queue=False)
-
+    
+    #corresponds to clicking the submit button
     sub_msg = subbtn.click(add_text, [openai_chat, txt], [openai_chat, txt], queue=False, api_name="submit").then(
         openai_bot, [openai_chat], openai_chat
     )
     sub_msg.then(lambda: gr.update(interactive=True), None, [txt], queue=False)
 
+    #hitting enter and clicking submit for email
     email_txt = emailtxt.submit(print_email, [emailtxt], [emailtxt], queue=False)
     email_msg = emailbtn.click(print_email, [emailtxt], [emailtxt], queue=False)
     

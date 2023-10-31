5b11d4ef452ae93b2949e8b05e7dc3c29795a560
Arman Aydemir <arman.aydemir@live.com>
2023-10-30 23:01:56 -0700

    chat conversion

--- app.py
+++ app.py
@@ -133,11 +133,11 @@ email_ga_script = """
 
 #script for chat submission event google analytics
 chat_ga_script = """
-(chat) => {
+(chat, text) => {
     gtag('event', 'chat_submission', {
       'chat_history_length': chat.length,
     })
-    return chat
+    return 
 }
 """
 
@@ -192,17 +192,15 @@ with gr.Blocks(
     #connecting frontend interactions to backend
 
     #corresponds to enter in the text box
-    txt_msg = txt.submit(add_text, [openai_chat, txt], [openai_chat, txt], queue=False).then(
-        lambda x: x, openai_chat, openai_chat, _js=chat_ga_script
-    ).then(
-        openai_bot, openai_chat, openai_chat
+    txt_msg = txt.submit(None, openai_chat, None, _js=chat_ga_script).then(
+        add_text, [openai_chat, txt], [openai_chat, txt], queue=False).then(
+        openai_bot, [openai_chat], [openai_chat]
     )
     txt_msg.then(lambda: gr.update(interactive=True), None, [txt], queue=False)
     
     #corresponds to clicking the submit button
-    sub_msg = subbtn.click(add_text, [openai_chat, txt], [openai_chat, txt], queue=False, api_name="submit").then(
-        lambda x: x, [openai_chat], [openai_chat], _js=chat_ga_script
-    ).then(
+    sub_msg = subbtn.click(None, openai_chat, None, _js=chat_ga_script).then(
+        add_text, [openai_chat, txt], [openai_chat, txt], queue=False, api_name="submit").then(
         openai_bot, [openai_chat], [openai_chat]
     )
     sub_msg.then(lambda: gr.update(interactive=True), None, [txt], queue=False)

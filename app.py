c9a55439e75fe11dd544c9ef806d08a8fbea6b0e
Arman Aydemir <arman.aydemir@live.com>
2023-11-16 01:23:13 -0800

    output parsing

--- app.py
+++ app.py
@@ -416,19 +416,18 @@ with gr.Blocks(
                 match = re.search(regex, llm_output, re.DOTALL)
                 if not match:
                     print('inside no match')
-                    q.put(llm_output.split("Final Answer:")[-1])
+                    q.put(llm_output.split("Question:")[-1])
                     # raise ValueError(f"Could not parse LLM output: `{llm_output}`")
                     return AgentFinish(
                         # Return values is generally always a dictionary with a single `output` key
                         # It is not recommended to try anything else at the moment :)
-                        return_values={"output": llm_output.split("Final Answer:")[-1]},
+                        return_values={"output": llm_output.split("Question:")[-1]},
                         log=llm_output,
                     )
                 action = match.group(1).strip()
                 action_input = match.group(2)
                 # Return the action and action input
-                print('inside return action')
-                q.put("Using tool:" + action)
+                q.put("Processing...")
                 return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
 
         prompt_template = CustomPromptTemplate(

fe202fc9c829f4e482a556b3547144d765b86d23
Arman Aydemir <arman.aydemir@live.com>
2023-12-01 15:19:08 -0800

    directly copy tutorial

---
+++ youtube.py
@@ -0,0 +1,54 @@
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import (
+    OpenAIWhisperParser,
+    OpenAIWhisperParserLocal,
+)
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+
+# set a flag to switch between local and remote parsing
+# change this to True if you want to use local parsing
+local = False
+
+# Two Karpathy lecture videos
+urls = ["https://youtu.be/kCc8FmEb1nY", "https://youtu.be/VMj-3S1tku0"]
+
+# Directory to save audio files
+save_dir = "./"
+
+# Transcribe the videos to text
+if local:
+    loader = GenericLoader(
+        YoutubeAudioLoader(urls, save_dir), OpenAIWhisperParserLocal()
+    )
+else:
+    loader = GenericLoader(YoutubeAudioLoader(urls, save_dir), OpenAIWhisperParser())
+docs = loader.load()
+
+
+# Combine doc
+combined_docs = [doc.page_content for doc in docs]
+text = " ".join(combined_docs)
+
+# Split them
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
+splits = text_splitter.split_text(text)
+
+# Build an index
+embeddings = OpenAIEmbeddings()
+vectordb = FAISS.from_texts(splits, embeddings)
+
+# Build a QA chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0),
+    chain_type="stuff",
+    retriever=vectordb.as_retriever(),
+)
+
+# Ask a question!
+query = "Why do we need to zero out the gradient before backprop at each step?"
+print(qa_chain.run(query))

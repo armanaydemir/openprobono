89ed019510a3d4de58d1f9e263a0f8b6eb3d59a4
Arman Aydemir <arman.aydemir@live.com>
2023-12-08 10:19:23 -0800

    try height

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ with gr.Blocks(
         return history, gr.update(value="", interactive=False)
 
     gr.Markdown("OpenProBono")
-    with gr.Row() as the_row:
+    with gr.Row(css="{max-height: 100%!important; height: 100%!important;}") as the_row:
         with gr.Column(scale=2) as chat_col:
             with gr.Row() as chat_row:
                 openai_chat = gr.Chatbot(

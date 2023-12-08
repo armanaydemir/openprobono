46e10e88da2df5ae814696ab472b3d392a6781f9
Arman Aydemir <arman.aydemir@live.com>
2023-12-08 12:07:50 -0800

    height al

--- app.py
+++ app.py
@@ -150,15 +150,15 @@ with gr.Blocks(
     footer {visibility: hidden}
     .gradio-container {max-width: 100%!important; width: 100%!important; max-height: 100vh!important; height: 100vh!important;}
     #therow {height: 100vh!important;}
-    #component-0 { height: 100%!important; }
+    #component-0 { height: 100vh!important; }
     #tools_col {height: 90%!important;}
     #chat_col {height: 90%!important;}
-    
+    #chatbot {height: 100%!important;}
     #chatrow { flex-grow: 3; }
     #inputrow { flex-grow: 1; }
     #clearopenai {flex-grow: 1; }
     """,
-    # #chatbot {height: 100%!important;}
+    # 
     # .contain { display: flex; flex-direction: column; }
     # component-0 { height: 100%; }
     # chatbot { flex-grow: 1; overflow: auto;}

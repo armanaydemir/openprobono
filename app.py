703e392405e1ca0bbbea5dadd7607383940408d9
Arman Aydemir <arman.aydemir@live.com>
2023-12-13 16:30:03 -0800

    user agent

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ with gr.Blocks(
     request = gr.Request()
     print(request)
     print("this is reqeust")
-    print(request['headers'])
+    print(request.client)
 
     session = gr.State(get_uuid_id)
 

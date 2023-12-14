a875ea78e60964fc6f7cf25bc789a6c5b5bbe6ac
Arman Aydemir <arman.aydemir@live.com>
2023-12-14 14:56:15 -0800

    fix hiding

--- app.py
+++ app.py
@@ -304,16 +304,16 @@ with gr.Blocks(
                 with gr.Tab("Details"):
                     gr.Markdown("OpenProBono AI is designed to assist users in finding relevant information and resources related to government and laws. While we strive to provide accurate and up-to-date information, it is important to note that the AI's results should be verified against official sources. The AI's findings should not be considered legal advice, and users should consult with legal professionals for specific legal matters. Additionally, the AI's recommendations and suggestions are based on algorithms and data analysis, and may not cover all possible scenarios or legal interpretations. The AI's developers and operators do not assume any liability for the accuracy, completeness, or reliability of the AI's results. Users are responsible for independently verifying the information and using their own judgment in making legal decisions. Learn more at www.openprobono.com.")
 
-                with gr.Row() as email_row:    
-                    emailtxt = gr.Textbox(
-                        scale=4,
-                        label="input",
-                        show_label=False,
-                        placeholder="Enter your email to sign up for updates",
-                        container=False,
-                        type="email",
-                    )
-                    emailbtn = gr.Button("Submit")
+            with gr.Row() as email_row:    
+                emailtxt = gr.Textbox(
+                    scale=4,
+                    label="input",
+                    show_label=False,
+                    placeholder="Enter your email to sign up for updates",
+                    container=False,
+                    type="email",
+                )
+                emailbtn = gr.Button("Submit")
     
     ##----------------------- backend   (llm stuff)-----------------------##
     def openai_bot(history, t1name, t1txt, t1prompt, t2name, t2txt, t2prompt, user_prompt, session):
@@ -578,7 +578,7 @@ with gr.Blocks(
 
     def isMobile_change(isMobile):
         return gr.update(visible=(not isMobile), render=(not isMobile), interactive=(not isMobile)), gr.update(visible=isMobile, render=isMobile, interactive=isMobile)
-    isMobile.change(isMobile_change, [isMobile], tools_desktop_group, queue=False)
+    isMobile.change(isMobile_change, [isMobile], [tools_desktop_group, tools_mobile_group], queue=False)
 
     #loading google analytics script
     app.load(None, None, None, _js=ga_script)

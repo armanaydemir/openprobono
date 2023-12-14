40b3672142ce65903ae65977745e759864e4d916
Arman Aydemir <arman.aydemir@live.com>
2023-12-14 14:52:39 -0800

    fix hiding

--- app.py
+++ app.py
@@ -206,7 +206,19 @@ with gr.Blocks(
                     container=False,
                 )
                 subbtn = gr.Button("Submit", variant="primary")
+
+        with gr.Group(visible=False, render=False, interactive=False) as tools_mobile_group:
+            with gr.Accordion("Examples", open=False):
+                for prompt in example_prompts:
+                    with gr.Accordion(prompt, open=False):
+                        for example in example_prompts[prompt]:
+                            exbtn = gr.Button(example)
+                            exbtn.click(lambda x: x, exbtn, txt, queue=False)
+
                 
+            with gr.Accordian("Details", open=False):
+                gr.Markdown("OpenProBono AI is designed to assist users in finding relevant information and resources related to government and laws. While we strive to provide accurate and up-to-date information, it is important to note that the AI's results should be verified against official sources. The AI's findings should not be considered legal advice, and users should consult with legal professionals for specific legal matters. Additionally, the AI's recommendations and suggestions are based on algorithms and data analysis, and may not cover all possible scenarios or legal interpretations. The AI's developers and operators do not assume any liability for the accuracy, completeness, or reliability of the AI's results. Users are responsible for independently verifying the information and using their own judgment in making legal decisions. Learn more at www.openprobono.com.")
+
             with gr.Row() as email_row:    
                 emailtxt = gr.Textbox(
                     scale=4,
@@ -217,18 +229,6 @@ with gr.Blocks(
                     type="email",
                 )
                 emailbtn = gr.Button("Submit")
-
-        with gr.Group() as tools_mobile_group:
-            with gr.Accordion("Examples", open=False):
-                for prompt in example_prompts:
-                    with gr.Accordion(prompt, open=False):
-                        for example in example_prompts[prompt]:
-                            exbtn = gr.Button(example)
-                            exbtn.click(lambda x: x, exbtn, txt, queue=False)
-
-                
-            with gr.Tab("Details", open=False):
-                gr.Markdown("OpenProBono AI is designed to assist users in finding relevant information and resources related to government and laws. While we strive to provide accurate and up-to-date information, it is important to note that the AI's results should be verified against official sources. The AI's findings should not be considered legal advice, and users should consult with legal professionals for specific legal matters. Additionally, the AI's recommendations and suggestions are based on algorithms and data analysis, and may not cover all possible scenarios or legal interpretations. The AI's developers and operators do not assume any liability for the accuracy, completeness, or reliability of the AI's results. Users are responsible for independently verifying the information and using their own judgment in making legal decisions. Learn more at www.openprobono.com.")
 
         with gr.Group() as tools_desktop_group:
             with gr.Column(scale=0, elem_id="tools_col_css") as tools_col:
@@ -577,7 +577,7 @@ with gr.Blocks(
     )
 
     def isMobile_change(isMobile):
-        return gr.update(visible=(not isMobile), render=(not isMobile), interactive=(not isMobile))
+        return gr.update(visible=(not isMobile), render=(not isMobile), interactive=(not isMobile)), gr.update(visible=isMobile, render=isMobile, interactive=isMobile)
     isMobile.change(isMobile_change, [isMobile], tools_desktop_group, queue=False)
 
     #loading google analytics script

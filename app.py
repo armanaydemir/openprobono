27b3cb59bd89fb19b446dde275fedf47e83e1cbe
Arman Aydemir <arman.aydemir@live.com>
2023-12-13 17:05:29 -0800

    user agent mobile check

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ with gr.Blocks(
     # """,
     analytics_enabled=False
     ) as app:
-    user_agent = gr.Checkbox(label="isMobile", visible=True, render=True)
+    user_agent = gr.Checkbox(label="isMobile", visible=False, render=False)
     #loading user agent
     app.load(None, None, [user_agent], _js=user_agent_script)
 
@@ -208,7 +208,7 @@ with gr.Blocks(
                 subbtn = gr.Button("Submit", variant="primary") 
             clearopenai = gr.ClearButton([txt, openai_chat], elem_id="clearopenai")
 
-        with gr.Column(scale=0, elem_id="tools_col", visible=(not user_agent)) as tools_col:
+        with gr.Column(scale=0, elem_id="tools_col", visible=(not user_agent.value())) as tools_col:
             with gr.Tab("Examples"):
                 for prompt in example_prompts:
                     with gr.Accordion(prompt, open=False):

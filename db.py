5d4d45837bb9df4dc14949460e657c50a6350ab9
Arman Aydemir <arman.aydemir@live.com>
2023-12-30 16:53:04 -0800

    db.py

--- db.py
+++ db.py
@@ -9,4 +9,4 @@ db = firestore.client()
 
 #getting conversations and showing most recent ones
 
-print(db.collection('conversations').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(10).get())
+print(db.collection('conversationsClone').limit(10).get())

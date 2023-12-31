6b3a838c34dd3a2694d10519a6c8d978ff8385ae
Arman Aydemir <arman.aydemir@live.com>
2023-12-30 17:42:53 -0800

    db.py

--- db.py
+++ db.py
@@ -8,12 +8,16 @@ db = firestore.client()
 
 
 #getting conversations and showing most recent ones
+past = datetime.now() - timedelta(days=4)
 
 docs = db.collection('conversationsClone').limit(10).get()
 for doc in docs:
-    print(f'{doc.id} => {doc.to_dict()}')
+    # print(f'{doc.id} => {doc.to_dict()}')
     subdocs = db.collection('conversationsClone').document(doc.id).collection('conversations').get()
-    print(subdocs)
+    # print(subdocs)
     for msg in subdocs:
-        print(f'{msg.id} => {msg.to_dict()}')
+        # print(f'{msg.id} => {msg.to_dict()}')
+        if(msg.to_dict()['timestamp'] > past):
+            print(f'{msg.id} => {msg.to_dict()}')
+            
     print("------------------")

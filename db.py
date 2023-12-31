94a51f5d17fcf8081edf58c057bcdc5968c080ce
Arman Aydemir <arman.aydemir@live.com>
2023-12-30 16:52:03 -0800

    db.py;

---
+++ db.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("../../creds.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+
+#getting conversations and showing most recent ones
+
+print(db.collection('conversations').order_by('timestamp', direction=firestore.Query.DESCENDING).limit(10).stream())
